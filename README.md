3a51fa38385253542f007e876ddff29361cb4bbc
Phillip Toohill <phillip.toohill@rackspace.com>
2013-11-05 16:37:21 -0600

    Updates to support PCRE validation

--- README.md
+++ README.md
@@ -1,13 +1,28 @@
-A simple tool to test Java and Perl regex validation. 
+A simple tool to test Java, Perl and PCRE regex validation.
+
+Compile: mvn -e clean compile assembly:single
+Compile C: make; make install
+           Then place on your path.
+
+Run: java -jar pregex-{version}-jar-with-dependencies.jar
 
 Usage: 
 
+Validates regex against Java, Perl and PCRE libraries.
+
 help: Returns this help menu.
 Example usage: <regex=^*$> <mstr=200>
-type: Use j or p for 'Java' or 'Perl' regex validation. Defaults to perl validation
+type: Use 'j' or 'p' for Java or Perl regex validation or 'pcre' for PCRE validation. Defaults to Perl validation
 regex: The regex to test with. 
 mstr: The string to match against. 
 You can also run this program in interactive mode by not supplying any arguments.
-Interactive usage: $> <type> <regex> <mstr> 
-Example: $> p ^*$ 200 :: type and regex are required. 
+Interactive usage: $: <type> <regex> <mstr>
+Example: $: p ^*$ 200 :: type and regex are required.
 While in interactive mode type 'help' to display this menu or 'exit' to quit.
+
+PCRE validation uses libpcre and will make a call to the compiled PCRE C application which inturn uses libpcre to validate the regex.
+Java will validate using the current Javax Pattern class.
+Perl will use JREGEX which is a application built using Perl 5.6 syntax and rules.
+     (Caution: there are additional rules allowed with this library that may not be accepted in regexp of Perl.)
+
+WARNING: not compiling and placing the C 'pcrevalidate' application in your path without supplying a 'type' you will receive errors!
--- src/main/java/com/pregex/Pregex.java
+++ src/main/java/com/pregex/Pregex.java
@@ -1,8 +1,7 @@
 package com.pregex;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
@@ -37,9 +36,10 @@ public class Pregex {
     }
 
     private static void prntHelp() {
-        System.out.println("help: Returns this help menu. \n" +
+        System.out.println("Validates regex against Java, Perl and PCRE libraries. \n\n" +
+                "help: Returns this help menu. \n\n" +
                 "Example usage: <regex=^*$> <mstr=200> \n\n" +
-                "type: Use j or p for 'Java' or 'Perl' regex validation. Defaults to perl validation \n" +
+                "type: Use 'j' or 'p' for Java or Perl regex validation or 'pcre' for PCRE validation. Defaults to Perl validation \n" +
                 "regex: The regex to test with. \n" +
                 "mstr: The string to match against. \n\n" +
                 "You can also run this program in interactive mode by not supplying any arguments. \n" +
@@ -64,8 +64,8 @@ public class Pregex {
                     try {
                         type = s.split(" ")[0];
                         iregex = s.split(" ")[1];
-                    } catch (ArrayStoreException aex) {
-                        System.out.println("Please provide type and regex values as they are required.");
+                    } catch (Exception aex) {
+                        System.out.println("Please provide type and regex values as they are required. EX: $: p ^*$");
                     }
                     try {
                         imstr = s.split(" ")[2];
@@ -81,43 +81,85 @@ public class Pregex {
         }
     }
 
-    private static void validate() {
-        if (type.equalsIgnoreCase("j")) {
-            try {
-                Pattern re = Pattern.compile(iregex);
-                System.out.println(re + " is a valid Java regex pattern!");
-                if (imstr != null) {
-                    Matcher matcher = re.matcher(imstr);
-                    // check all occurance
-                    while (matcher.find()) {
-                        System.out.println("Text: " + "'" + imstr + "'" + " matched for Java validator!");
-                        System.out.println("Start index: " + matcher.start());
-                        System.out.println("End index: " + matcher.end());
-                        System.out.println(matcher.group());
-                    }
-                }
-            } catch (PatternSyntaxException exception) {
-                System.err.println("Failed during Java regex validation: ");
-                System.err.println(exception.getDescription());
-            }
-        } else {
-            //perl regex validation
-            try {
-                jregex.Pattern re = new jregex.Pattern(iregex);
-                System.out.println(re + " is a valid Perl regex pattern!");
-                if (imstr != null) {
-                    jregex.Matcher matcher = re.matcher(imstr);
-                    // check all occurance
-                    while (matcher.find()) {
-                        System.out.println("Text: " + "'" + imstr + "'" + " matched for Perl validator!");
-                        System.out.println("Start index: " + matcher.start());
-                        System.out.println("End index: " + matcher.end());
-                    }
-                }
-            } catch (jregex.PatternSyntaxException exception) {
-                System.err.println("Failed during Perl regex validation: ");
-                System.err.println(exception.getMessage());
-            }
+    private static void validate() throws IOException {
+        if (type.equalsIgnoreCase("pcre") && iregex != "") {
+            vPcre();
+        } else if (type.equalsIgnoreCase("j") && iregex != "") {
+            vJava();
+        } else if (iregex != "") {
+            vPerl();
         }
+    }
+
+    private static void vJava() {
+        try {
+            Pattern re = Pattern.compile(iregex);
+            System.out.println(re + " is a valid Java regex pattern!");
+            if (imstr != null) {
+                Matcher matcher = re.matcher(imstr);
+                // check all occurance
+                while (matcher.find()) {
+                    System.out.println("Text: " + "'" + imstr + "'" + " matched for Java validator!");
+                    System.out.println("Start index: " + matcher.start());
+                    System.out.println("End index: " + matcher.end());
+                    System.out.println(matcher.group());
+                }
+            }
+        } catch (PatternSyntaxException exception) {
+            System.err.println("Failed during Java regex validation: ");
+            System.err.println(exception.getDescription());
+        }
+    }
+
+    private static void vPerl() {
+        //perl regex validation
+        try {
+            jregex.Pattern re = new jregex.Pattern(iregex);
+            System.out.println(re + " is a valid Perl regex pattern!");
+            if (imstr != null) {
+                jregex.Matcher matcher = re.matcher(imstr);
+                // check all occurance
+                while (matcher.find()) {
+                    System.out.println("Text: " + "'" + imstr + "'" + " matched for Perl validator!");
+                    System.out.println("Start index: " + matcher.start());
+                    System.out.println("End index: " + matcher.end());
+                }
+            }
+        } catch (jregex.PatternSyntaxException exception) {
+            System.err.println("Failed during Perl regex validation: ");
+            System.err.println(exception.getMessage());
+        }
+    }
+
+    private static void vPcre() {
+        Process cPgm = null;
+        try {
+            cPgm = Runtime.getRuntime().exec("pcrevalidate");
+
+            OutputStream stdin = cPgm.getOutputStream();
+            InputStream rd = cPgm.getInputStream();
+            stdin.write(iregex.getBytes());
+            stdin.flush();
+            stdin.close();
+            InputStreamReader reader = new InputStreamReader(rd);
+            Scanner scan = new Scanner(reader);
+            String resp = scan.nextLine();
+            scan.close();
+            reader.close();
+            rd.close();
+
+            cPgm.destroy();
+
+            String status = resp.split("\\|")[0];
+            String msg = resp.split("\\|")[1];
+            if (status.equals("BAD")) {
+                System.out.println("Regex is invalid: " + msg);
+            } else {
+                System.out.println("Regex is valid: " + msg);
+            }
+        } catch (IOException e) {
+            System.out.println("PCRE Validation failed due to IOException: " + e.getMessage());
+        }
+
     }
 }
